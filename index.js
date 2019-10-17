c8526ebc08ade5a6331944bc2be3a9f6262a4bcc
Allan Pichardo <allan.pichardo@gmail.com>
2019-10-17 16:27:18 -0400

    Added custom storage scheme for Multer so we can save images

--- index.js
+++ index.js
@@ -5,16 +5,39 @@ const multer  = require('multer');
 const app = express();
 
 const publicDirectory = path.join(__dirname, 'public');
-const productImageUploads = multer({ dest: path.join(publicDirectory, 'products')});
-const profileImageUploads = multer({dest: path.join(publicDirectory, 'profiles')});
+const productImageUploads = multer({storage: getProductImageStorage()});
+const profileImageUploads = multer({storage: getProfileImageStorage()});
 
 const port = process.env.PORT || 3030;
 app.use(express.static(publicDirectory));
 
-/* Add your routes here */
+/*-------------- Add your routes here --------------*/
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-
+/* -----------------Config Stuff------------------- */
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
+
+function getProductImageStorage() {
+    return multer.diskStorage({
+        destination: path.join(publicDirectory, 'products'),
+        filename: function (req, file, cb) {
+            crypto.pseudoRandomBytes(16, function (err, raw) {
+                cb(null, raw.toString('hex') + Date.now() + '.' + mime.extension(file.mimetype));
+            });
+        }
+    });
+}
+
+function getProfileImageStorage() {
+    return multer.diskStorage({
+        destination: path.join(publicDirectory, 'profiles'),
+        filename: function (req, file, cb) {
+            crypto.pseudoRandomBytes(16, function (err, raw) {
+                cb(null, raw.toString('hex') + Date.now() + '.' + mime.extension(file.mimetype));
+            });
+        }
+    });
+}
