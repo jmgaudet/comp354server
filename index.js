82a964ab8aa47394064563b3cdecba60ab076923
Allan Pichardo <allan.pichardo@gmail.com>
2019-10-17 15:41:48 -0400

    Using multer for saving images to either products or profiles

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const multer  = require('multer');
 const app = express();
-const port = process.env.PORT || 3030;
 
-/* Serve any static files in the public directory */
 const publicDirectory = path.join(__dirname, 'public');
+const productImageUploads = multer({ dest: path.join(publicDirectory, 'products')});
+const profileImageUploads = multer({dest: path.join(publicDirectory, 'profiles')});
+
+const port = process.env.PORT || 3030;
 app.use(express.static(publicDirectory));
 
 /* Add your routes here */
