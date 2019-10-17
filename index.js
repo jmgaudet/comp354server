d1787c70009b8e83e70ffdb2e548a0c0bedb2962
Allan Pichardo <allan.pichardo@gmail.com>
2019-10-17 14:24:16 -0400

    Added public directory to serve static files

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3030;
 
+/* Serve any static files in the public directory */
+const publicDirectory = path.join(__dirname, 'public');
+app.use(express.static(publicDirectory));
+
+/* Add your routes here */
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
